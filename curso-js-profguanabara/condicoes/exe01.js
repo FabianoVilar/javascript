a42f8e69c7f929d34c0052d59e08bdc077720bd7
FabianoVilar <fabianoribeiro.vg@gmail.com>
2024-01-01 20:31:43 -0300

    Atualização javascript

---
+++ curso-js-profguanabara/condicoes/exe01.js
@@ -0,0 +1,12 @@
+var vel = 70
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+
+if (vel > 60) { // se houver if é uma condição simples
+  console.log(`Você ultrapassou a velocidade permitidade. MULTADO!`)
+}
+
+else {
+  console.log('Você está dentro da velocidade permitida')
+}
+
+console.log('Dirija sempre utilizando o cinto de segurança')
---
+++ curso-js-profguanabara/condicoes/exe02.js
@@ -0,0 +1,17 @@
+var pais = 'USA'
+console.log(`Vivendo no ${pais}`)
+if (pais == 'Brasil') {
+  console.log(`Você é Brasileiro`)
+}
+else {
+  console.log(`Você é estrangeiro`)
+}
+ /*Também poderia ser feito desta maneira abaixo utilizando o sinal de diferente:
+
+ var pais = 'USA'
+console.log(`Vivendo no ${pais}`)
+if (pais != 'Brasil') {
+  console.log(`Você é estrangeiro`)
+}
+else {
+  console.log(`Você é brasileiro`)*/
