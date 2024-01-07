3b60d2d35289727381077fce2b5d390c828fe4eb
FabianoVilar <fabianoribeiro.vg@gmail.com>
2024-01-07 14:40:09 -0300

    Atualização javaScript

--- curso-js-profguanabara/exercicios-js-profguanabara/modelo-exe/script.js
+++ curso-js-profguanabara/exercicios-js-profguanabara/modelo-exe/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function carregar() {
+  var msg = window.document.getElementById('msg')
+  var img = window.document.getElementById('img')
+  var data = new Date()
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas`
+  if (hora >= 0 && hora < 12) {
+    // Bom dia
+    img.src = 'fotomanha.png'
+    document.body.style.background = '#fee0b7'
+  } else if (hora >= 12 && hora < 18) {
+    // Boa tarde
+    img.src = 'fototarde.png'
+    document.body.style.background = '#ffa044'
+  } else {
+    // Boa noite
+    img.src = 'fotonoite.png'
+    document.body.style.background = '#133c71'
+  }
+}
